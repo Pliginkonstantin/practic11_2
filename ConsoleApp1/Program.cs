eb8050d630158cccfc44d39016fcf8efe4331795
Pliginkonstantin <falsekiller3@gmail.com>
2024-12-09 20:23:48 +0200

    Task1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// Задача 1. Поменять местами содержимое двух строковых переменных.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string strOne = "это первая строка ";
+            string strTwo = "это вторая строка";
+            Console.WriteLine($"вывод до обмена : {strOne} , {strTwo}");
+            Swap(ref strOne, ref strTwo);
+            Console.WriteLine($"вывод после обмена : {strOne} , {strTwo}");
+            Console.Read();
+        }
+        static void Swap(ref string strOne,ref string strTwo) 
+        {
+            string strNope = strOne;
+            strOne = strTwo;
+            strTwo = strNope;
+        }
+    }
+}
